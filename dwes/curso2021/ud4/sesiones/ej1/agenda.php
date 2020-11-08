a048b30da1aee6605e387fc8d5836c2f57919b33
Alvaro L <alvarooleto@gmail.com>
2020-11-08 20:48:55 +0100

    sesiones parte 1

--- dwes/curso2021/ud4/sesiones/ej1/agenda.php
+++ dwes/curso2021/ud4/sesiones/ej1/agenda.php
@@ -1,14 +1,61 @@
 <?php
+session_start();
+
+$nombre;
+$numero;
+// $arrayContactos = array();
+$agenda = "<table><tr><th colspan=3>AGENDA</th></tr>";
+
+if (!isset($_SESSION["arrayContactos"])) {
+    $_SESSION["arrayContactos"] = array();
+}
+
 function anadirContacto()
 {
+    $nombre = $_POST["nombre"];
+    $numero = $_POST["numero"];
+
+    $nuevoContacto = array(
+        array(
+            "nombre" => $nombre,
+            "numero" => $numero
+        )
+    );
+
+    $_SESSION["arrayContactos"] = array_merge($nuevoContacto, $_SESSION["arrayContactos"]);
 }
 
-function mostrarAgenda()
+function mostrarAgenda($agenda)
 {
+    if (!empty($_SESSION["arrayContactos"])) {
+        // print_r($_SESSION["arrayContactos"]);
+        foreach ($_SESSION["arrayContactos"] as $contacto) {
+            $agenda .= "<tr>";
+            foreach ($contacto as $key => $value) {
+                $agenda .= "<td>$value</td>";
+            }
+            $agenda .= "<td><button name='boton" . $_SESSION['arrayContactos'][0]['nombre'] . "' id='boton" . $_SESSION['arrayContactos'][0]['nombre'] . "' onclick='eliminarContacto(\"boton" . $_SESSION['arrayContactos'][0]['nombre'] . "\")'><img src='pictures/eliminar.png'></button></td></tr>";
+        }
+        echo $agenda;
+
+
+    } else {
+        echo "<p>La agenda está vacía.</p>";
+    }
 }
 
-function borrarAgenda()
-{
+function eliminarContacto($contacto) {
+    echo $contacto;
+
+    // NO CONSIGO QUE SE EJECUTE ESTA FUNCION DESDE LA LINEA 37
+
+    // foreach ($_SESSION["arrayContactos"] as $contacto) {
+        
+    //     foreach ($contacto as $key => $value) {
+            
+    //     }
+        
+    // }
 }
 ?>
 
@@ -24,6 +71,19 @@ function borrarAgenda()
         input {
             margin: 5px;
         }
+
+        table,
+        tr,
+        th,
+        td {
+            border: 1px solid black;
+            text-align: center;
+            padding: 5px;
+        }
+
+        img {
+            width: 25px;
+        }
     </style>
 </head>
 
@@ -36,15 +96,14 @@ function borrarAgenda()
         <input type="text" name="numero" id="numero"></br>
         <input type="submit" name="anadir" value="Añadir contacto">
         <input type="submit" name="mostrar" value="Mostrar agenda">
-        <input type="submit" name="borrar" value="Eliminar todos los contactos">
+        <!-- <input type="submit" name="borrar" value="Eliminar todos los contactos"> -->
+        <a href="cerrarSesion.php">Borrar todos los contactos</a>
     </form>
     <?php
     if (isset($_POST["anadir"])) {
         anadirContacto();
     } else if (isset($_POST["mostrar"])) {
-        echo mostrarAgenda();
-    } else if (isset($_POST["borrar"])) {
-        borrarAgenda();
+        echo mostrarAgenda($agenda);
     }
     ?> <?php
         echo "<div id='codigo'><a href='../../../verCodigo.php?src=" . __FILE__ . "'><button>Ver Código</button></a></div>";
