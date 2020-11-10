6f891245b6ed17a53381120350c7a189b11e74d1
Alvaro L <alvarooleto@gmail.com>
2020-11-10 10:14:58 +0100

    empezando ej2 sesiones

---
+++ dwes/curso2021/ud4/sesiones/ej2/gestionAcademica.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+$nombre;
+$nota;
+$tablaNotas = "<table><tr><th colspan=4>tablaNotas</th></tr>";
+
+if (!isset($_SESSION["arrayAlumnos"])) {
+    $_SESSION["arrayAlumnos"] = array(
+        array(
+            "id" => 1,
+            "nombre" => "",
+            "nota" => ""
+        ),
+        array(
+            "id" => 2,
+            "nombre" => "",
+            "nota" => ""
+        )
+    );
+}
+
+function anadirAlumno()
+{
+    
+}
+
+function eliminarAlumno($contactoABorrar)
+{
+    
+}
+
+function mostrarNotas($tablaNotas)
+{
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ej2 - Gestión académica</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+    <style>
+        input {
+            margin: 5px;
+        }
+
+        table,
+        tr,
+        th,
+        td {
+            border: 1px solid black;
+            text-align: center;
+            padding: 5px;
+        }
+
+        img {
+            width: 25px;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>Gestión académica</h2>
+    <form action="tablaNotas.php" method="post">
+        <label for="nombre">Nombre: </label>
+        <input type="text" name="nombre" id="nombre"></br>
+        <label for="nota">Nota en DWES: </label>
+        <input type="text" name="nota" id="nota"></br>
+        <input type="submit" name="anadir" value="Añadir alumno">
+        <input type="submit" name="mostrar" value="Mostrar notas">
+        <a href="cerrarSesion.php">Borrar la sesión</a>
+    </form>
+    <?php
+    if (isset($_POST["anadir"])) {
+        anadirAlumno();
+    } else if (isset($_GET["el"])) {
+        eliminarAlumno($_GET["el"]);
+    } else if (isset($_POST["mostrar"])) {
+        echo mostrarNotas($tablaNotas);
+    }
+    ?> <?php
+        echo "<div id='codigo'><a href='../../../verCodigo.php?src=" . __FILE__ . "'><button>Ver Código</button></a></div>";
+        ?>
+</body>
+
+</html>
