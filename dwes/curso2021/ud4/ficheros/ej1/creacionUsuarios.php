5d7a7af1188d37535137d97692cbb1c0f6d831f8
Alvaro L <alvarooleto@gmail.com>
2020-11-25 14:15:25 +0100

    ej1 ficheros terminado

--- dwes/curso2021/ud4/ficheros/ej1/creacionUsuarios.php
+++ dwes/curso2021/ud4/ficheros/ej1/creacionUsuarios.php
@@ -1,6 +1,102 @@
 <?php
 $arrayImagenes = array();
 $archivo;
+$contenidoActual;
+$contenido;
+
+function crearArchivoMysql($nombre, $apellido)
+{
+    if (!is_file("archivos/mysql.txt")) {
+        $archivo = fopen("archivos/mysql.txt", "a+");
+
+        $contenido = "create database mibbdd
+                        CREATE TABLE mitabla{
+                            id MEDIUMINT NOT NULL AUTO_INCREMENT,
+                            nombre CHAR(20) NOT NULL,
+                            apellido CHAR(40) NOT NULL,
+                            PRIMARY KEY (id);
+                        }
+                        INSERT INTO mitabla (nombre, apellido) VALUES
+                        ('$nombre', '$apellido')";
+
+        fwrite($archivo, $contenido);
+
+        fclose($archivo);
+    } else {
+        $archivo = fopen("archivos/mysql.txt", "a+");
+
+        $contenido = "\n
+                        INSERT INTO mitabla (nombre, apellido) VALUES
+                        ('$nombre', '$apellido')";
+
+        fwrite($archivo, $contenido);
+
+        fclose($archivo);
+    }
+}
+
+function crearArchivoOracle($nombre, $apellido)
+{
+    if (!is_file("archivos/oracle.txt")) {
+        $archivo = fopen("archivos/oracle.txt", "a+");
+
+        $contenido = "create database mibbdd
+                 DATAFILE filespec AUTOEXTEND ON [NEXT int K | M] [MAXSIZE int K | M]
+                        CREATE TABLE mitabla{
+                            id NUMBER (9),
+                            nombre VARCHAR(20),
+                            apellido VARCHAR(40),
+                            PRIMARY KEY (id);
+                        }
+                        INSERT INTO mitabla (nombre, apellido) VALUES
+                        ('$nombre', '$apellido')";
+
+        fwrite($archivo, $contenido);
+
+        fclose($archivo);
+    } else {
+        $archivo = fopen("archivos/oracle.txt", "a+");
+
+        $contenido = "\n
+                        INSERT INTO mitabla (nombre, apellido) VALUES
+                        ('$nombre', '$apellido')";
+
+        fwrite($archivo, $contenido);
+
+        fclose($archivo);
+    }
+}
+
+function crearArchivoLinux($nombre, $apellido)
+{
+    if (!is_file("archivos/linux.txt")) {
+        $archivo = fopen("archivos/linux.txt", "a+");
+
+        $contenido = "create database mibbdd
+                        CREATE TABLE mitabla{
+                            id MEDIUMINT NOT NULL AUTO_INCREMENT,
+                            nombre CHAR(20) NOT NULL,
+                            apellido CHAR(40) NOT NULL,
+                            PRIMARY KEY (id);
+                        }
+                        INSERT INTO mitabla (nombre, apellido) VALUES
+                        ('$nombre', '$apellido')";
+
+        fwrite($archivo, $contenido);
+
+        fclose($archivo);
+    } else {
+        $archivo = fopen("archivos/linux.txt", "a+");
+
+        $contenido = "\n
+                        INSERT INTO mitabla (nombre, apellido) VALUES
+                        ('$nombre', '$apellido')";
+
+        fwrite($archivo, $contenido);
+
+        fclose($archivo);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +108,8 @@ $archivo;
     <title>Ej1 - Script de creación de usuarios</title>
     <link rel="stylesheet" type="text/css" href="css/estilos.css" />
     <style>
-        label, input {
+        label,
+        input {
             margin: 5px 0;
         }
     </style>
@@ -35,7 +132,22 @@ $archivo;
     </form>
     <?php
     if (isset($_POST["crear"])) {
-        
+        if ((isset($_POST["nombre"]) && ($_POST["apellido"]))) {
+            if (isset($_POST["option1"])) {
+                crearArchivoMysql($_POST["nombre"], $_POST["apellido"]);
+                echo "<p>Datos introducidos en BD de MySQL correctamente.</p>";
+            }
+            if (isset($_POST["option2"])) {
+                crearArchivoOracle($_POST["nombre"], $_POST["apellido"]);
+                echo "<p>Datos introducidos en BD de Oracle correctamente.</p>";
+            }
+            if (isset($_POST["option3"])) {
+                crearArchivoLinux($_POST["nombre"], $_POST["apellido"]);
+                echo "<p>Datos introducidos en BD de Linux correctamente.</p>";
+            }
+        } else {
+            echo "Nombre y apellido no pueden estas vacíos.";
+        }
     } else {
         echo "<p>Hubo algún error al crear el fichero.</p>";
     }
