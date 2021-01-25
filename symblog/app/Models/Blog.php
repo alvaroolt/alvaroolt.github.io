d9bc1e3bdc789217d939e4e90ceeedeba4c8b7ae
Alvaro L <alvarooleto@gmail.com>
2021-01-25 10:12:52 +0100

    symblog - parte 1

---
+++ symblog/app/Models/Blog.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Clase Blog
+ * @author Álvaro Leiva Toledano
+ */
+
+class Blog
+{
+    public $id;
+    public $title;
+    public $author;
+    public $blog;
+    public $image;
+    public $tags;
+    public $created;
+    public $updated;
+
+    // public function __construct($arrayDatos)
+    // {
+    //     $this->id = $arrayDatos['id'];
+    //     $this->title = $arrayDatos['title'];
+    //     $this->author = $arrayDatos['author'];
+    //     $this->blog = $arrayDatos['blog'];
+    //     $this->image = $arrayDatos['image'];
+    //     $this->tags = $arrayDatos['tags'];
+    //     $this->created = $arrayDatos['created'];
+    //     $this->updated = $arrayDatos['updated'];
+    // }
+
+    // public function getId()
+    // {
+    //     return $this->id;
+    // }
+
+    // public function setId($id)
+    // {
+    //     $this->id = $id;
+    // }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function getBlog()
+    {
+        return $this->blog;
+    }
+
+    public function setBlog($blog)
+    {
+        $this->blog = $blog;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    public function addComment($comment) {
+        // no se jaja
+    }
+}
---
+++ symblog/app/Models/Comment.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Clase Comment
+ * @user Álvaro Leiva Toledano
+ */
+
+class Comment
+{
+    public $id;
+    public $blog_id;
+    public $user;
+    public $comment;
+    public $approved;
+    public $created;
+    public $updated;
+
+    public function getBlog()
+    {
+        return $this->blog_id;
+    }
+
+    public function setBlog($blog_id)
+    {
+        $this->blog_id = $blog_id;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getApproved()
+    {
+        return $this->approved;
+    }
+
+    public function setApproved($approved)
+    {
+        $this->approved = $approved;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+}
