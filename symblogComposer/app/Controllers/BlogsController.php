a06cd50891b3aa7e65ace32fe9d9467ec4bf4045
Alvaro L <alvarooleto@gmail.com>
2021-02-14 16:23:18 +0100

    symblog - practica 14

--- symblogComposer/app/Controllers/BlogsController.php
+++ symblogComposer/app/Controllers/BlogsController.php
@@ -15,6 +15,16 @@ class BlogsController extends BaseController
             $blog->blog = $postData["description"];
             $blog->tags = $postData["tags"];
             $blog->author = $postData["author"];
+
+            // Carga de archivos
+            $files = $request->getUploadedFiles();
+            $imagen = $files['imagen'];
+            if ($imagen->getError() == UPLOAD_ERR_OK) {
+                $fileName = $imagen->getClientFilename();
+                $fileName = uniqid() . $fileName;
+                // $imagen->moveTO("../public/img/$fileName");
+                $blog->imagen = $fileName;
+            }
             $blog->save();
         }
         return $this->renderHTML("addBlog.twig");
