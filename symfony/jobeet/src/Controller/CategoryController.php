d2ff68b9368e7aeb6195159bc855f0983c7df17d
Alvaro L <alvarooleto@gmail.com>
2021-02-28 15:23:50 +0100

    symfony - dia 8

---
+++ symfony/jobeet/src/Controller/CategoryController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Job;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Knp\Component\Pager\PaginatorInterface;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * Finds and displays a category entity.
+     *
+     * @Route("/category/{slug}", name="category.show", methods="GET")
+     *
+     * @param Category $category
+     * @param PaginatorInterface $paginator
+     *
+     * @return Response
+     */
+    public function show(Category $category, PaginatorInterface $paginator) : Response
+    {
+        $activeJobs = $paginator->paginate(
+            $this->getDoctrine()->getRepository(Job::class)->getPaginatedActiveJobsByCategoryQuery($category),
+            1, // page
+            10 // elements per page
+        );
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'activeJobs' => $activeJobs,
+        ]);
+    }
+}
