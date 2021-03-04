6d8d5771078a062a6c1ece404391e50eb71f1567
Alvaro L <alvarooleto@gmail.com>
2021-03-04 15:58:51 +0100

    examen - arreglo bloqueo 3 intentos de inicio de sesión

--- examen/include/errorLogin.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($_SESSION["intentos"] == 3) { 
-    echo "Usuario bloqueado";
-} else {
-    echo "Error al iniciar sesión. Te quedan " . 3-$_SESSION["intentos"] . " intentos."; 
-}
--- examen/index.php
+++ examen/index.php
@@ -1,7 +1,7 @@
 <?php
 include "config/config.php";
 include "class/Usuario.php";
-include "class/Log.php";
+// include "class/Log.php";
 include "class/Obra.php";
 
 session_start();
@@ -9,48 +9,54 @@ session_start();
 if (!isset($_SESSION["intentos"])) {
     $_SESSION["intentos"] = 0;
 }
-if (!isset($_SESSION["passEr"])) {
-    $_SESSION["passEr"] = "";
+if (!isset($_SESSION["userErr"])) {
+    $_SESSION["userErr"] = "";
 }
-// echo $_SESSION["intentos"];
+echo $_SESSION["intentos"] . "</br>";
+echo $_SESSION["userErr"] . "</br>";
 
 if (!isset($_SESSION["perfil"])) {
     $_SESSION["usuario"] = Usuario::getInstancia();
     $_SESSION["obra"] = Obra::getInstancia();
     // print_r($_SESSION["obra"]);
     // print_r($_SESSION["usuario"]);
-    $_SESSION["log"] = Log::getInstancia();
+    // $_SESSION["log"] = Log::getInstancia();
     $_SESSION["perfil"] = "invitado";
 }
 if (isset($_POST["login"])) {
     $arrayUsuario = $_SESSION["usuario"]->get($_POST["user"]);
-    $arrayLog = array();
+    // $arrayLog = array();
     if (sizeof($arrayUsuario) == 1) {
         $_SESSION["id_usuario"] = $arrayUsuario[0]["id"];
         $_SESSION["user"] = $arrayUsuario[0]["usuario"];
-        array_push($arrayLog, $_SESSION["user"]);
+        // array_push($arrayLog, $_SESSION["user"]);
         if ($arrayUsuario[0]["password"] == $_POST["pass"]) {
             $_SESSION["perfil"] = $arrayUsuario[0]["perfiles_perfil"];
             $_SESSION["password"] = $arrayUsuario[0]["password"];
             $_SESSION["correo"] = $arrayUsuario[0]["email"];
             $_SESSION["estado"] = $arrayUsuario[0]["bloqueado"];
-            array_push($arrayLog, "Ha iniciado sesión");
+            $_SESSION["intentos"] = 0;
+            $_SESSION["userErr"] = "";
+            // array_push($arrayLog, "Ha iniciado sesión");
         } else {
             $_SESSION["intentos"]++;
-            echo $_SESSION["intentos"];
-            if ($_SESSION["passEr"] == $_POST["pass"]) {
+            if ($_SESSION["user"] == $_SESSION["userErr"]) {
                 if ($_SESSION["intentos"] == 3) {
                     $_SESSION["usuario"]->desactivarUser($_SESSION["id_usuario"]);
-                    $_SESSION["intentos"] = 0;
+                    echo "El usuario " . $_SESSION["user"] . " ha sido desactivado.";
+                } else {
+                    echo "Error al iniciar sesión. Te quedan " . (3 - $_SESSION["intentos"]) . " intentos.";
                 }
-                // include "include/errorLogin.php";
             } else {
-                $_SESSION["passEr"] = $_POST["pass"];
-                $_SESSION["intentos"] = 0;
+                if ($_SESSION["userErr"] !== "") {
+                    $_SESSION["intentos"] = 1;
+                }
+                $_SESSION["userErr"] = $_SESSION["user"];
+                echo "Error al iniciar sesión. Te quedan " . (3 - $_SESSION["intentos"]) . " intentos.";
             }
-            array_push($arrayLog, "Inicio de sesión erróneo");
+            // array_push($arrayLog, "Inicio de sesión erróneo");
         }
-        $_SESSION["log"]->set($arrayLog);
+        // $_SESSION["log"]->set($arrayLog);
         if ($_SESSION["perfil"] == "gerente") {
             header("Location: index.php?page=gerente");
         } else if ($_SESSION["perfil"] == "amigo") {
